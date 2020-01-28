fad53dc773b6e8254edad1e5d3c4a4b6974f225c
Bo Wang <wangbo1995ee@tamu.edu>
2020-01-28 23:18:07 +0000

    part1 && part2 finished, no bugs

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -4,28 +4,73 @@
 
 def sum arr
   # YOUR CODE HERE
+  arr.inject(0, :+)
 end
 
 def max_2_sum arr
   # YOUR CODE HERE
+  arr.sort!.reverse!
+  range = [2, arr.length].min
+  sum = 0
+  for i in 0...range do
+    sum += arr[i]
+  end
+  sum
 end
 
 def sum_to_n? arr, n
   # YOUR CODE HERE
+  hash_val = Hash.new
+  arr.each_with_index { |val, idx|
+    if hash_val[n - val] != nil && hash_val[n - val] != idx
+      return true
+    end
+    hash_val[val] = idx
+  }
+  false
 end
 
 # Part 2
 
 def hello(name)
   # YOUR CODE HERE
+  "Hello, #{name}"
+end
+
+def letter?(lookAhead)
+  lookAhead =~ /[[:alpha:]]/
 end
 
 def starts_with_consonant? s
   # YOUR CODE HERE
+  if s == "" || !letter?(s[0])
+    return false
+  end
+  vowel = ['a', 'e', 'i', 'o', 'u']
+  first = s[0].dup.downcase
+  vowel.each { |vo|
+  if vo == first 
+    return false
+  end
+  }
+  true
+end
+
+
+def number_or_nil(string)
+  Integer(string || '')
+rescue ArgumentError
+  nil
 end
 
 def binary_multiple_of_4? s
   # YOUR CODE HERE
+  num = number_or_nil(s)
+  if (num == nil || num % 4 != 0)
+    return false
+  else
+    return true
+  end
 end
 
 # Part 3
